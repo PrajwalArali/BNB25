1278fc1d006f08ccd202a2e898ac3b23c6519748
Prajwal Arali <147275460+PrajwalArali@users.noreply.github.com>
2025-09-14 11:31:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // or './frontend' if your config is in a different location
+  root: './frontend', // or './frontend' if your config is in a different location
   build: {
     outDir: 'dist',
   }
